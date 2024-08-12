3667bf412de7b8516ae4724b0837d29bf57ee0fa
SakuraBabochka <sakurababochka@gmail.com>
2024-08-12 13:50:55 +0100

    Added block and item registers. Added Snakewood Planks.

---
+++ src/main/java/com/majesticstudios/item/ModItems.java
@@ -0,0 +1,16 @@
+package com.majesticstudios.item;
+
+import com.majesticstudios.majesticdragons.MajesticDragons;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, MajesticDragons.MOD_ID);
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
--- src/main/java/com/majesticstudios/majesticdragons/MajesticDragons.java
+++ src/main/java/com/majesticstudios/majesticdragons/MajesticDragons.java
@@ -1,10 +1,13 @@
 package com.majesticstudios.majesticdragons;
 
+import com.majesticstudios.block.ModBlocks;
+import com.majesticstudios.item.ModItems;
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -25,6 +28,11 @@ public class MajesticDragons {
     public static final String MOD_ID = "majesticdragons";
 
     public MajesticDragons() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
+
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 
